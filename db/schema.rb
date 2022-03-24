6327cea3161e335ba34269768cbed19a42949bbc
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-03-24 15:10:41 +0100

    feat: add facilities table to database
    Via:
    bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_24_131112) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_24_135611) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_24_131112) do
     t.decimal "latitude"
     t.decimal "longitude"
     t.string "district"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "focus_groups", force: :cascade do |t|
+    t.string "label"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
