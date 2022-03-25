e74855dbd37f263461ece6b03fe4ccf631d09c6a
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-03-25 11:21:05 +0100

    feat: seed facilities into database
    Via:
    bin/rails db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,43 @@
-FocusGroup.insert_all([
-                        { label: 'Erwachsene' },
-                        { label: 'Suchterkrankte' },
-                        { label: 'Geflüchtete' },
-                        { label: 'Migrationshintergrund' },
-                        { label: 'Kinder und Jugendliche' },
-                        { label: 'Essstörungen' },
-                        { label: 'Frauen' },
-                        { label: 'LSTBIQ' },
-                        { label: 'HIV/AIDS-Erkrankte' }
-                      ])
+require 'csv'
+
+# headers: true because there is a header row present in the source CSV
+# converters: %i[numeric] makes sure that any numeric value (integer or float) is casted from the default CSV entry string
+# col_sep: ';' tells the parser which character separates the columns
+CSV.foreach(
+  Rails.root.join('lib/data/data.csv'),
+  headers: true,
+  converters: %i[numeric],
+  col_sep: ';',
+) do |row|
+  # Create a new facility for every row in the CSV
+  Facility.create(
+    {
+      title: row['Projekt'],
+      provider: row['Zuwendungsempfänger'],
+      service: row['Leistung'],
+      address: row['Adresse'],
+      postal_code: row['PLZ'],
+      city: row['Ort'],
+      phone: row['Telefonnummer'],
+      email: row['EMail'],
+      website: row['Website'],
+      latitude: row['gc_ywert'],
+      longitude: row['gc_xwert'],
+      district: row['Bezirk'],
+    },
+  )
+end
+
+FocusGroup.insert_all(
+  [
+    { label: 'Erwachsene' },
+    { label: 'Suchterkrankte' },
+    { label: 'Geflüchtete' },
+    { label: 'Migrationshintergrund' },
+    { label: 'Kinder und Jugendliche' },
+    { label: 'Essstörungen' },
+    { label: 'Frauen' },
+    { label: 'LSTBIQ' },
+    { label: 'HIV/AIDS-Erkrankte' },
+  ],
+)
