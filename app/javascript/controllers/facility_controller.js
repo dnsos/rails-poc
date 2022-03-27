53bc3f75e3210440b8eb00ccd41b8ddafb217d73
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-03-27 21:18:11 +0200

    refactor: use map utils in facilities#show

--- app/javascript/controllers/facility_controller.js
+++ app/javascript/controllers/facility_controller.js
@@ -1,14 +1,21 @@
 import { Controller } from "@hotwired/stimulus";
+import { getMaptilerStyle } from "./utils/map_utils.js";
 import "maplibre-gl";
 
 export default class extends Controller {
+  static values = {
+    "maptiler-key": String,
+  };
+
   connect() {
+    if (!this.hasMaptilerKeyValue) return;
+
     const latitude = Number(this.element.dataset.latitude);
     const longitude = Number(this.element.dataset.longitude);
 
     this.map = new maplibregl.Map({
       container: "facility-map",
-      style: `https://api.maptiler.com/maps/streets/style.json?key=`, // TODO: hide the key!
+      style: getMaptilerStyle(this.maptilerKeyValue),
       center: [longitude, latitude],
       zoom: 16,
     });
