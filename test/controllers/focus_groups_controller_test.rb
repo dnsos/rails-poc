8b2ea2445b3d9399909cd9634ffaf7db478dfe17
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-03-28 18:59:42 +0200

    test: fix test for focus group controller

--- test/controllers/focus_groups_controller_test.rb
+++ test/controllers/focus_groups_controller_test.rb
@@ -1,8 +1,10 @@
 require 'test_helper'
 
 class FocusGroupsControllerTest < ActionDispatch::IntegrationTest
+  setup { @focus_group = focus_groups(:one) }
+
   test 'should get show' do
-    get focus_groups_show_url
+    get focus_group_url(@focus_group)
     assert_response :success
   end
 end
