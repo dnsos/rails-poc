8685c91d3055554c0983e6d3373af600391819fe
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-03-30 19:32:42 +0200

    feat: add role to user table (and default to user)
    Other role is admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ApplicationRecord
+  # Currently we only have two roles:
+  enum role: %i[user admin]
+  after_initialize :set_default_role, if: :new_record?
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable
+
+  private
+
+  def set_default_role
+    self.role ||= :user
+  end
 end
