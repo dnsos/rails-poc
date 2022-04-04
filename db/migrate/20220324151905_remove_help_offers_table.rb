b90fc836cf2dedfc2e42809a39558b9275cc4c51
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-04-04 20:06:29 +0200

    chore: remove mistakenly kept migration

--- db/migrate/20220324151905_remove_help_offers_table.rb
+++ db/migrate/20220324151905_remove_help_offers_table.rb
@@ -1,5 +1,7 @@
 class RemoveHelpOffersTable < ActiveRecord::Migration[7.0]
   def change
-    drop_table :help_offers
+    # This migration was mistakenly kept.
+    # We do not trigger it anymore.
+    # drop_table :help_offers
   end
 end
