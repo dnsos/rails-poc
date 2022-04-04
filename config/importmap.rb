ed0589456e97a58e01504413b6ac4ffc90f958ae
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-04-04 20:29:03 +0200

    refactor: make map_utils accessible by importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,12 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application", preload: true
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers"
+pin 'application', preload: true
+pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
+pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
+pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
+pin_all_from 'app/javascript/controllers', under: 'controllers'
 
-pin 'maplibre-gl', to: 'https://unpkg.com/maplibre-gl@1.15.2/dist/maplibre-gl.js'
+pin_all_from 'app/javascript/utils', under: 'utils'
+
+pin 'maplibre-gl',
+    to: 'https://unpkg.com/maplibre-gl@1.15.2/dist/maplibre-gl.js'
